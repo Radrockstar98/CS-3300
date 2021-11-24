ad54a1bb8091835cadd96f6b4ddfc6c9526f2121
Chad Bowman <chadbowman1909@gmail.com>
2021-11-24 18:19:38 +0000

    adding addtional test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, :type => :model do
+  subject {
+      User.new(email: "nomail@fakemail.com",
+               password: "LeaveOutAllTheRest")
+  }
+  
+  it "is valid with an email and password" do
+    expect(subject).to be_valid  
+  end
+
+  it "is not valid without an email address" do
+    subject.email = nil
+    expect(subject).to_not be_valid  
+  end  
+  
+  it "is no valid without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid  
+  end  
 end
